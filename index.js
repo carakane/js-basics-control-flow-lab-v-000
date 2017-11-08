e0bc13121461009601549ba37b124eb44b7e9982
Cara Kane <cara.kane@gmail.com>
2017-11-08 16:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
     return('This one is on me!');
-  } else if (ride > 2000) {
-    return('I will gladly take your thirty bucks.');
   } else if (ride > 2500) {
     return('No can do.');
+  } else if (ride > 2000) {
+    return('I will gladly take your thirty bucks.');
   }
 }
