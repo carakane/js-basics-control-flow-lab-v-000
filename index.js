bcd6abafbc21337ec0b6c02c49f09c495c355509
Cara Kane <cara.kane@gmail.com>
2017-11-08 16:55:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function scuberGreetingForFeet(ride) {
 }
 
 function ternaryCheckCity(city) {
-  (city === 'NYC') ? "Ok, sounds good." : "No go."
+  city === 'NYC' ? return "Ok, sounds good." : return "No go."
 }
